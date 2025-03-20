cfc8ae8dcfa0f4fa6aa57318311ca9d796f92da1
Anton Litvin <antonlitvin2006@gmail.com>
2025-03-20 15:16:56 +0300

    testing parser 3

---
+++ parser_rigla_ru.py
@@ -0,0 +1,67 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+import os
+import time
+import json
+
+def scrape_rigla_ru(search_query):
+    """
+    Парсит данные с сайта Apteka.ru по заданному запросу.
+
+    :param search_query: Поисковый запрос (например, "аспирин")
+    :return: Данные в формате JSON
+    """
+    # Указываем путь к chromedriver
+    chromedriver_path = os.path.join(os.getcwd(), "chromedriver")
+
+    # Создаем объект Service
+    service = Service(executable_path=chromedriver_path)
+
+    # Инициализация драйвера с использованием Service
+    driver = webdriver.Chrome(service=service)
+
+    try:
+        # 1. Переход на страницу
+        driver.get(f"https://apteka.ru/search/?q={search_query}")
+
+        # 2. Ожидание появления окна выбора города и его закрытие
+        try:
+            WebDriverWait(driver, 5).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "TownSelector"))
+            )
+            body = driver.find_element(By.TAG_NAME, "body")
+            body.send_keys(Keys.ESCAPE)
+            print("Окно выбора города закрыто с помощью клавиши 'Esc'.")
+        except Exception as e:
+            print("Окно выбора города не найдено или не удалось закрыть:", e)
+
+        # 3. Ожидание 10 секунд (дополнительное время для загрузки страницы)
+        time.sleep(10)
+
+        # 4. Извлечение текста из всех доступных блоков
+        product_names = driver.find_elements(By.CLASS_NAME, "product__title")
+        product_prices = driver.find_elements(By.CLASS_NAME, "product__active-price-number")
+        product_images = driver.find_elements(By.CLASS_NAME, "product__img")
+
+        # 5. Формирование данных в виде списка словарей
+        products = []
+        for name, price, image in zip(product_names, product_prices, product_images):
+            product = {
+                "name": name.get_attribute("title"),
+                "price": price.text,
+                "image_url": image.get_attribute("src")
+            }
+            products.append(product)
+
+
+        # 6. Преобразование в JSON
+        result = json.dumps(products)
+        return result
+    finally:
+        # Закрытие драйвера
+        driver.quit()
+
